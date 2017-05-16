f5fd2b2c9e9bbaaa8f0fcde3a695b0950778dd24
Thai Pangsakulyanont <dtinth@spacet.me>
2017-05-16 23:29:23 +0700

    :sparkle: Add web font

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,22 @@ export function MainLayout ({ children }) {
     <div>
       <Head />
       <style global jsx>{`
+        @font-face {
+          font-family: 'ThaiSans Neue';
+          src: local('ThaiSans Neue'),
+               url('static/thaisansneue/thaisansneue-bold-webfont.woff2') format('woff2'),
+               url('static/thaisansneue/thaisansneue-bold-webfont.woff') format('woff');
+          font-weight: bold;
+          font-style: normal;
+        }
+        @font-face {
+          font-family: 'thaisans_neueregular';
+          src: local('ThaiSans Neue'),
+               url('static/thaisansneue/thaisansneue-regular-webfont.woff2') format('woff2'),
+               url('static/thaisansneue/thaisansneue-regular-webfont.woff') format('woff');
+          font-weight: normal;
+          font-style: normal;
+        }
         body {
           margin: 0;
           background: #222;
