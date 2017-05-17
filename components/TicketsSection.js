f2d3ffbc560aa2b74ab5dbcabfc7225733c2e02a
Thai Pangsakulyanont <dtinth@spacet.me>
2017-05-18 03:15:45 +0700

    :pencil2: Tickets button -> Get Tickets

--- components/TicketsSection.js
+++ components/TicketsSection.js
@@ -5,7 +5,7 @@ export default function TicketsSection () {
     <div className='tickets'>
       <SectionTitle>Tickets</SectionTitle>
       <TicketsTable />
-      <Button href='https://www.eventpop.me/e/1809-react-bangkok-2-0-0'>Tickets</Button>
+      <Button href='https://www.eventpop.me/e/1809-react-bangkok-2-0-0'>Get Tickets</Button>
       <style jsx>{`
         .tickets {
           text-align: center;
