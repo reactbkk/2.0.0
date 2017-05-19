093d9ce959f296086c7d716518f9bcb6e378c740
Worapot Pengsuk <worapot@dek-d.com>
2017-05-19 17:34:37 +0700

    fix lint

--- static/service-worker-registration.js
+++ static/service-worker-registration.js
@@ -1,8 +1,9 @@
-'use strict';
+/* global __NEXT_DATA__ */
+'use strict'
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    if( typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.nextExport === true ){
+  window.addEventListener('load', function () {
+    if (typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.nextExport === true) {
       navigator.serviceWorker.register('/2.0.0/service-worker.js')
     }
-  });
+  })
 }
