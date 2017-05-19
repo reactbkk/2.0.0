4cd7361e67883221ddbe69cde3839b1a5dfcfa86
Worapot Pengsuk <worapot@dek-d.com>
2017-05-19 17:09:58 +0700

    remove .then from service-worker-registration

--- static/service-worker-registration.js
+++ static/service-worker-registration.js
@@ -2,26 +2,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     if( typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.nextExport === true ){
-      navigator.serviceWorker.register('/2.0.0/service-worker.js').then(function(reg) {
-        reg.onupdatefound = function() {
-          var installingWorker = reg.installing;
-          installingWorker.onstatechange = function() {
-            switch (installingWorker.state) {
-              case 'installed':
-                if (navigator.serviceWorker.controller) {
-                  // console.log('New or updated content is available.');
-                } else {
-                  // console.log('Content is now available offline!');
-                }
-                break;
-              case 'redundant':
-                break;
-            }
-          };
-        };
-      }).catch(function(e) {
-        // console.error('Error during service worker registration:', e);
-      });
+      navigator.serviceWorker.register('/2.0.0/service-worker.js')
     }
   });
 }
