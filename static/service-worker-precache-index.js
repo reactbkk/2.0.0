a1844977ebdb2a418faeb11963d6d765bf134559
Thai Pangsakulyanont <dtinth@spacet.me>
2017-05-19 18:59:05 +0700

    :bug: :wrench: Make sure index.html is precached by sw-toolbox
    To allow network-first behavior

---
+++ static/service-worker-precache-index.js
@@ -0,0 +1,4 @@
+toolbox.precache([
+  '/2.0.0/',
+  '/2.0.0/index.html'
+])
--- sw-precache-config.js
+++ sw-precache-config.js
@@ -6,6 +6,9 @@ module.exports = {
   stripPrefix: 'out/',
   replacePrefix: '/2.0.0/',
   verbose: true,
+  importScripts: [
+    'static/service-worker-precache-index.js'
+  ],
   runtimeCaching: [{
     urlPattern: /(.*)/,
     handler: 'networkFirst'
