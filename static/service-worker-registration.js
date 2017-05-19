5efa0fea407d45eded062607e9859233431c8650
Worapot Pengsuk <worapot@dek-d.com>
2017-05-19 15:53:08 +0700

    load service-worker on gh-page only

--- static/service-worker-registration.js
+++ static/service-worker-registration.js
@@ -1,25 +1,27 @@
 'use strict';
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/2.0.0/service-worker.js').then(function(reg) {
-      reg.onupdatefound = function() {
-        var installingWorker = reg.installing;
-        installingWorker.onstatechange = function() {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // console.log('New or updated content is available.');
-              } else {
-                // console.log('Content is now available offline!');
-              }
-              break;
-            case 'redundant':
-              break;
-          }
+    if( typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.nextExport === true ){
+      navigator.serviceWorker.register('/2.0.0/service-worker.js').then(function(reg) {
+        reg.onupdatefound = function() {
+          var installingWorker = reg.installing;
+          installingWorker.onstatechange = function() {
+            switch (installingWorker.state) {
+              case 'installed':
+                if (navigator.serviceWorker.controller) {
+                  // console.log('New or updated content is available.');
+                } else {
+                  // console.log('Content is now available offline!');
+                }
+                break;
+              case 'redundant':
+                break;
+            }
+          };
         };
-      };
-    }).catch(function(e) {
-      // console.error('Error during service worker registration:', e);
-    });
+      }).catch(function(e) {
+        // console.error('Error during service worker registration:', e);
+      });
+    }
   });
 }
