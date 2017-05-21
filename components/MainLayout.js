24a6858133e96d58b7062494361a0bf067e8138a
serm tape <serm.thm@gmail.com>
2017-05-21 14:28:10 +0700

    Fix lint
    Disable 2 rules in MainLayoutPage
    Added aria-hidden attribute in SpeakersSection

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,3 +1,9 @@
+/*
+eslint
+  jsx-a11y/click-events-have-key-events: "off",
+  jsx-a11y/no-static-element-interactions: "off",
+*/
+
 import Head from 'next/head'
 import { scroller } from 'react-scroll'
 
