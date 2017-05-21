edfa84877e510721afd7a38b043f5eef25ecd8e2
chanwit piromplad <kingkong2103@gmail.com>
2017-05-21 10:38:08 +0700

    fix follow lint

--- pages/_document.js
+++ pages/_document.js
@@ -13,34 +13,34 @@ const ga = `
 export default class MyDocument extends Document {
   render () {
     return (
-     <html lang='th'>
+      <html lang='th'>
         <Head>
-            <title>React Bangkok 2.0.0</title>
-            <meta
+          <title>React Bangkok 2.0.0</title>
+          <meta
             name='viewport'
             content='width=device-width, initial-scale=1.0'
-            />
-            <meta
+          />
+          <meta
             property='og:image'
             content='https://reactbkk.github.io/2.0.0/static/og-image.jpg'
-            />
-            <meta name='theme-color' content='#222' />
-            <meta name='apple-mobile-web-app-title' content='ReactBKK' />
-            <meta name='apple-mobile-web-app-capable' content='yes' />
-            <link rel='shortcut icon' type='image/png' href='static/favicon.png' sizes='32x32' />
-            <link rel='apple-touch-icon' href='https://reactbkk.github.io/2.0.0/static/icon-ios-fullsize.png' />
-            <link rel='apple-touch-icon' sizes='152x152' href='https://reactbkk.github.io/2.0.0/static/icon-ios-152.png' />
-            <link rel='apple-touch-icon' sizes='167x167' href='https://reactbkk.github.io/2.0.0/static/icon-ios-167.png' />
-            <link rel='apple-touch-icon' sizes='180x180' href='https://reactbkk.github.io/2.0.0/static/icon-ios-180.png' />
-            <script dangerouslySetInnerHTML={{ __html: ga }} />
-            <script defer src='static/service-worker-registration.js' />
-            <link rel='manifest' href='static/manifest.json' />
+          />
+          <meta name='theme-color' content='#222' />
+          <meta name='apple-mobile-web-app-title' content='ReactBKK' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <link rel='shortcut icon' type='image/png' href='static/favicon.png' sizes='32x32' />
+          <link rel='apple-touch-icon' href='https://reactbkk.github.io/2.0.0/static/icon-ios-fullsize.png' />
+          <link rel='apple-touch-icon' sizes='152x152' href='https://reactbkk.github.io/2.0.0/static/icon-ios-152.png' />
+          <link rel='apple-touch-icon' sizes='167x167' href='https://reactbkk.github.io/2.0.0/static/icon-ios-167.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='https://reactbkk.github.io/2.0.0/static/icon-ios-180.png' />
+          <script dangerouslySetInnerHTML={{ __html: ga }} />
+          <script defer src='static/service-worker-registration.js' />
+          <link rel='manifest' href='static/manifest.json' />
         </Head>
         <body>
-            <Main />
-            <NextScript />
+          <Main />
+          <NextScript />
         </body>
-     </html>
+      </html>
     )
   }
 }
