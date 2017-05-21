34515f8a694c90da856f1aae5b90b3763925a473
chanwit piromplad <kingkong2103@gmail.com>
2017-05-21 10:59:22 +0700

    move global style to _document.js

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -27,32 +27,6 @@ const handleLinkClick = (event) => {
 export default function MainLayout ({ children }) {
   return (
     <div onClick={handleLinkClick}>
-      <style global jsx>{`
-        @font-face {
-          font-family: 'ThaiSans Neue';
-          src: local('ThaiSans Neue'),
-              url('static/thaisansneue/thaisansneue-bold-webfont.woff2') format('woff2'),
-              url('static/thaisansneue/thaisansneue-bold-webfont.woff') format('woff');
-          font-weight: bold;
-          font-style: normal;
-        }
-        @font-face {
-          font-family: 'ThaiSans Neue';
-          src: local('ThaiSans Neue'),
-              url('static/thaisansneue/thaisansneue-regular-webfont.woff2') format('woff2'),
-              url('static/thaisansneue/thaisansneue-regular-webfont.woff') format('woff');
-          font-weight: normal;
-          font-style: normal;
-        }
-        body {
-          margin: 0;
-          background: #222;
-          color: #fff;
-          font-family: ThaiSans Neue;
-          font-size: 22px;
-          -webkit-font-smoothing: antialiased;
-        }
-      `}</style>
       {children}
     </div>
   )
