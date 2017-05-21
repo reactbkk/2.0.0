8d099997540ddb65100ce49bc48a935b912a53d8
wasu <khing9277@gmail.com>
2017-05-22 01:25:23 +0700

    fix lint

--- components/ScheduleSection.js
+++ components/ScheduleSection.js
@@ -1,88 +1,85 @@
-// Created By Wasu Winitmontri
-
 import SectionTitle from './SectionTitle'
 
 const sessions = [
   {
     id: 1,
     type: 'FLASH',
-    time: '10:00 - 10:10', 
-    session_1_topic: 'Welcome Speech by Venue', 
+    time: '10:00 - 10:10',
+    sessionFirstTopic: 'Welcome Speech by Venue'
   },
   {
     id: 2,
     type: 'MAIN',
-    time: '10:30 - 11:15', 
-    session_1_topic: 'Let\'s start width React in the right way', 
-    session_1_avatar: 'https://avatars2.githubusercontent.com/u/25113?v=3&s=400',
-    session_1_speaker: 'Somkiat Puisungnoen (somkiat.cc)',
-    session_2_topic: 'Scalable React app Architecture', 
-    session_2_avatar: 'https://avatars2.githubusercontent.com/u/134488?v=3&s=460',
-    session_2_speaker: 'Zack Siri (Founder at Artellectual)',
+    time: '10:30 - 11:15',
+    sessionFirstTopic: 'Let\'s start width React in the right way',
+    sessionFirstAvatar: 'https://avatars2.githubusercontent.com/u/25113?v=3&s=400',
+    sessionFirstSpeaker: 'Somkiat Puisungnoen (somkiat.cc)',
+    sessionSecondTopic: 'Scalable React app Architecture',
+    sessionSecondAvatar: 'https://avatars2.githubusercontent.com/u/134488?v=3&s=460',
+    sessionSecondSpeaker: 'Zack Siri (Founder at Artellectual)'
   },
   {
     id: 3,
     type: 'MAIN',
-    time: '11:15 - 12:00', 
-    session_1_topic: 'Live coding / mob programming session', 
-    session_1_avatar: 'https://avatars1.githubusercontent.com/u/193136?v=3&s=400',
-    session_1_speaker: 'Thai Pangsakulyanont (Task World)',
-    session_2_topic: 'Behavior Driven Development in React Native', 
-    session_2_avatar: 'https://avatars1.githubusercontent.com/u/19698625?v=3&s=400',
-    session_2_speaker: 'Tanakorn numrubporn (CTO at Planforfit)',
+    time: '11:15 - 12:00',
+    sessionFirstTopic: 'Live coding / mob programming session',
+    sessionFirstAvatar: 'https://avatars1.githubusercontent.com/u/193136?v=3&s=400',
+    sessionFirstSpeaker: 'Thai Pangsakulyanont (Task World)',
+    sessionSecondTopic: 'Behavior Driven Development in React Native',
+    sessionSecondAvatar: 'https://avatars1.githubusercontent.com/u/19698625?v=3&s=400',
+    sessionSecondSpeaker: 'Tanakorn numrubporn (CTO at Planforfit)'
   },
   {
     id: 4,
-    type:'BREAK',
-    time: 'Lunch Break', 
+    type: 'BREAK',
+    time: 'Lunch Break'
   },
   {
     id: 5,
     type: 'FLASH',
-    time: '13:00 - 14:10', 
-    session_1_topic: 'Lightning Talk', 
+    time: '13:00 - 14:10',
+    sessionFirstTopic: 'Lightning Talk'
   },
   {
     id: 6,
     type: 'MAIN',
-    time: '14:15 - 15:00', 
-    session_1_topic: 'Switching from Mobile to React Web Developer', 
-    session_1_avatar: 'http://upic.me/i/5h/18489682_10211021272026019_3559519120929813202_o.jpg',
-    session_1_speaker: 'Chinnawat Panmonkol (Wongnai)',
-    session_2_topic: 'Styled Components: Change your CSS styling mindset', 
-    session_2_avatar: 'http://upic.me/i/6u/18556898_1436098509744650_6799324924419962365_o.jpg',
-    session_2_speaker: 'Yanin Tuamsuk (Front-end Engineer at Jitta)',
+    time: '14:15 - 15:00',
+    sessionFirstTopic: 'Switching from Mobile to React Web Developer',
+    sessionFirstAvatar: 'http://upic.me/i/5h/18489682_10211021272026019_3559519120929813202_o.jpg',
+    sessionFirstSpeaker: 'Chinnawat Panmonkol (Wongnai)',
+    sessionSecondTopic: 'Styled Components: Change your CSS styling mindset',
+    sessionSecondAvatar: 'http://upic.me/i/6u/18556898_1436098509744650_6799324924419962365_o.jpg',
+    sessionSecondSpeaker: 'Yanin Tuamsuk (Front-end Engineer at Jitta)'
   },
   {
     id: 7,
-    type:'BREAK',
-    time: 'Break', 
+    type: 'BREAK',
+    time: 'Break'
   },
   {
     id: 8,
     type: 'MAIN',
-    time: '15:15 - 16:00', 
-    session_1_topic: 'Angualat to React without breakingthings', 
-    session_1_avatar: 'https://avatars3.githubusercontent.com/u/155392?v=3&s=400',
-    session_1_speaker: 'Jirat Kijlerdpornpailoj (SPRINT3r / Gucode)',
-    session_2_topic: 'Testabled React: Patterns and comparison', 
-    session_2_avatar: 'http://upic.me/t/b3/15965740_10154900014726797_4415870819464215979_n.jpg',
-    session_2_speaker: 'Chakrit Likitkhajorn (Lead Developer at Taskworld)',
+    time: '15:15 - 16:00',
+    sessionFirstTopic: 'Angualat to React without breakingthings',
+    sessionFirstAvatar: 'https://avatars3.githubusercontent.com/u/155392?v=3&s=400',
+    sessionFirstSpeaker: 'Jirat Kijlerdpornpailoj (SPRINT3r / Gucode)',
+    sessionSecondTopic: 'Testabled React: Patterns and comparison',
+    sessionSecondAvatar: 'http://upic.me/t/b3/15965740_10154900014726797_4415870819464215979_n.jpg',
+    sessionSecondSpeaker: 'Chakrit Likitkhajorn (Lead Developer at Taskworld)'
   },
   {
     id: 9,
     type: 'MAIN',
-    time: '16:00 - 16:45', 
-    session_1_topic: 'Workshop with Next.js', 
-    session_1_avatar: 'http://upic.me/t/3j/18485743_10213102449583027_7199744295355607870_n.jpg',
-    session_1_speaker: 'Nati Namvong (CTO at Sellsuki)',
-    session_2_topic: 'Lessons Learned from using Next.js in Production', 
-    session_2_avatar: 'http://upic.me/i/pb/18589022_10209316039524510_1628867117219649509_o.jpg',
-    session_2_speaker: 'Panjamapong (PanJ) Sermsawatsri (CTO at TakeMeTour)',
-  },
-  
-]
+    time: '16:00 - 16:45',
+    sessionFirstTopic: 'Workshop with Next.js',
+    sessionFirstAvatar: 'http://upic.me/t/3j/18485743_10213102449583027_7199744295355607870_n.jpg',
+    sessionFirstSpeaker: 'Nati Namvong (CTO at Sellsuki)',
+    sessionSecondTopic: 'Lessons Learned from using Next.js in Production',
+    sessionSecondAvatar: 'http://upic.me/i/pb/18589022_10209316039524510_1628867117219649509_o.jpg',
+    sessionSecondSpeaker: 'Panjamapong (PanJ) Sermsawatsri (CTO at TakeMeTour)'
+  }
 
+]
 
 export default function ScheduleSection () {
   return (
@@ -118,13 +115,13 @@ const Session = (props) => {
   const {
     type,
     time,
-    session_1_topic, 
-    session_1_speaker, 
-    session_1_avatar,
-    session_2_topic, 
-    session_2_speaker, 
-    session_2_avatar,
-  } = props;
+    sessionFirstTopic,
+    sessionFirstSpeaker,
+    sessionFirstAvatar,
+    sessionSecondTopic,
+    sessionSecondSpeaker,
+    sessionSecondAvatar
+  } = props
   return (
     <div className='session'>
       { type === 'MAIN' &&
@@ -133,14 +130,14 @@ const Session = (props) => {
           <div className='column-md-5 column-xs-12'>
             <div className='row'>
               <div className='column-md-2'>
-                <img className="speaker-avatar" src={session_1_avatar} />
+                <img className='speaker-avatar' src={sessionFirstAvatar} />
               </div>
               <div className='column-md-10'>
                 <div className='topic'>
-                  {session_1_topic}
+                  {sessionFirstTopic}
                 </div>
                 <div className='speaker'>
-                  {session_1_speaker}
+                  {sessionFirstSpeaker}
                 </div>
               </div>
             </div>
@@ -148,14 +145,14 @@ const Session = (props) => {
           <div className='column-md-5 column-xs-12'>
             <div className='row'>
               <div className='column-md-2'>
-                <img className="speaker-avatar" src={session_2_avatar} />
+                <img className='speaker-avatar' src={sessionSecondAvatar} />
               </div>
               <div className='column-md-10'>
                 <div className='topic'>
-                  {session_2_topic}
+                  {sessionSecondTopic}
                 </div>
                 <div className='speaker'>
-                  {session_2_speaker}
+                  {sessionSecondSpeaker}
                 </div>
               </div>
             </div>
@@ -173,7 +170,7 @@ const Session = (props) => {
         type === 'FLASH' &&
         <div className='row _margin-bottom-30'>
           <div className='column-md-2 column-xs-12 time'>{time}</div>
-          <div className='column-md-5 column-xs-12 main'>{session_1_topic}</div>
+          <div className='column-md-5 column-xs-12 main'>{sessionFirstTopic}</div>
         </div>
       }
       <style jsx>{`
