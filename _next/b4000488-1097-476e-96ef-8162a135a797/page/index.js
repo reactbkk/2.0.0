48642f865017876b52fc822ae266beeefd3c160f
CircleCI <circleci@spacet.me>
2017-05-22 04:51:36 +0000

    Deploy e8c53529d739f0e835c4f45a624d8569cd645535 to GitHub pages [ci skip]

--- _next/b4000488-1097-476e-96ef-8162a135a797/page/index.js
+++ _next/b4000488-1097-476e-96ef-8162a135a797/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([3],[],[239]);
+            var comp = module.exports=webpackJsonp([3],[],[240]);
             return { page: comp.default }
           })
         
