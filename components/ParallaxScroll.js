9f2d2007fd48ef9cc91a5c06e67795bf98cca0ec
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2017-05-23 23:57:06 +0700

    Fix rename function

--- components/ParallaxScroll.js
+++ components/ParallaxScroll.js
@@ -1,19 +1,20 @@
 export default class Parallax extends React.Component {
   componentDidMount () {
-    window.addEventListener('load', this.scrollLoop, false)
+    window.addEventListener('scroll', this.updateParallax, false)
   }
 
-  componentWillUnMount () {
-    window.removeEventListener('load', this.scrollLoop, false)
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.updateParallax, false)
   }
 
   setPosition = (yPos) => {
     this.dom.style.transform = `translate3d(0, ${yPos}px, 0)`
   }
 
-  scrollLoop = () => {
-    this.setPosition(window.scrollY * this.props.speed)
-    window.requestAnimationFrame(this.scrollLoop)
+  updateParallax = () => {
+    window.requestAnimationFrame(() => {
+      this.setPosition(window.scrollY * this.props.speed)
+    })
   }
 
   render () {
